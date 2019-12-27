9f02470184d2c24d39566472e4663b55a9b9666b
kelvinmuthama <kelvinkamau387@gmail.com>
2019-12-27 11:08:11 +0300

    Added a js file to calculate insurance

---
+++ mepip.js
@@ -0,0 +1,17 @@
+document.getElementById("insurance-btn").addEventListener("click", function() {
+
+    // Calculate Insurance
+
+    let loanGranted, numberOfMonths, insurance;
+    loanGranted = document.getElementById("loan-granted").value;
+    numberOfMonths = document.getElementById("months").value;
+
+    const calculateInsurance = () => {
+        insurance = ((5.03 * numberOfMonths + 3.03) * loanGranted) / 6000
+
+        return Math.round(insurance * 100) / 100
+    }
+
+    document.getElementById("insurance").innerHTML = calculateInsurance();
+    
+})
